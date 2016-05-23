f56446a8612d9839dcfe247782da607f840fd0c0
Mike Fossel <mikefossel@gmail.com>
2016-05-23 16:44:10 -0700

    added core class

---
+++ VehicleWebService/VehicleWebService.CORE/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿
+namespace VehicleWebService.CORE
+
+{
+    public class Vehicle
+    {
+
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+    }
+
+}
