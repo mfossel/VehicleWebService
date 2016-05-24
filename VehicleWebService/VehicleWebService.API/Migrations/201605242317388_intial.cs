4a4aadeb68d95445ec300548603a8691520c2f08
Mike Fossel <mikefossel@gmail.com>
2016-05-24 16:20:51 -0700

    migrate to database

---
+++ VehicleWebService/VehicleWebService.API/Migrations/201605242317388_intial.cs
@@ -0,0 +1,28 @@
+namespace VehicleWebService.API.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Vehicles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Year = c.Int(nullable: false),
+                        Make = c.String(),
+                        Model = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Vehicles");
+        }
+    }
+}
---
+++ VehicleWebService/VehicleWebService.API/VehicleWebServiceDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using VehicleWebService.CORE;
+
+namespace VehicleWebService.API
+{
+    public class VehicleWebServiceDataContext : DbContext
+    {
+
+        public VehicleWebServiceDataContext()
+        {
+        }
+
+        //SQL TABLES
+        public IDbSet<Vehicle> Vehicles { get; set; }
+
+
+    }
+
+}
