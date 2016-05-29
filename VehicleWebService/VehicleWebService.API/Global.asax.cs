3134fde405dcd43aa03ebfd10def273b821586a1
Mike Fossel <mikefossel@gmail.com>
2016-05-29 13:11:35 -0700

    connected to redis data store!!!

--- VehicleWebService/VehicleWebService.API/Global.asax.cs
+++ VehicleWebService/VehicleWebService.API/Global.asax.cs
@@ -1,17 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using ServiceStack.Redis;
+using System;
+using System.Reflection;
 using System.Web.Http;
-using System.Web.Routing;
+using VehicleWebService.API.Data;
 
 namespace VehicleWebService.API
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public IRedisClientsManager ClientsManager;
+        private const string RedisUri = "CmlTJScXSQDqn5sz3HcC+kzGFDFrtMpBIr3Ycmi0U+A=@VehicleDataStore.redis.cache.windows.net?ssl=true";
+
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            ClientsManager = new PooledRedisClientManager(RedisUri);
+
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            ConfigureDependencyResolver(GlobalConfiguration.Configuration);
+
+            GlobalConfiguration.Configuration.EnsureInitialized();
         }
+
+        private void ConfigureDependencyResolver(HttpConfiguration configuration)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly())
+                .PropertiesAutowired();
+
+            builder.RegisterType<VehicleRepository>()
+                .As<IVehicleRepository>()
+                .PropertiesAutowired()
+                .InstancePerApiRequest();
+
+            builder.Register<IRedisClient>(c => ClientsManager.GetClient())
+                .InstancePerApiRequest();
+
+            configuration.DependencyResolver
+                = new AutofacWebApiDependencyResolver(builder.Build());
+        }
+
+        protected void Application_OnEnd()
+        {
+            ClientsManager.Dispose();
+        }
+
     }
 }
